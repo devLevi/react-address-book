a9400f92cd9e5d8a87e4337389b4e6aa74367f84
Levi Bernard <levichristianbernard@gmail.com>
2018-10-05 15:20:06 -0600

    added starter files for address book drill

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AddressBook from './components/address-book';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<AddressBook />, document.getElementById('root'));
